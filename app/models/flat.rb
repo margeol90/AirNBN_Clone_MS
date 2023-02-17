61aba7ff957581e201568b9e0eb8e979f5b0be72
Babajide Muritala <jide142000@yahoo.com>
2023-02-17 08:20:18 +0100

    reviews validations and route set

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,4 @@
 class Flat < ApplicationRecord
   belongs_to :user
-
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Adding an 'about' controller method
-  resources :flats
+  resources :flats do
+    resources :reviews, only: [:new]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
