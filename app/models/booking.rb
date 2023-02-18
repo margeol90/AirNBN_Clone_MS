fd3cc2ae7e881ed08a4e0243f3e4799934d91548
Margarita Dagla <margaritadagla@hotmail.com>
2023-02-18 11:14:59 +0000

    tweaks to deploy on heroku

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :flat
 
   validates start_date, presence: true
-  validates end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates end_date, presence: true
 end
