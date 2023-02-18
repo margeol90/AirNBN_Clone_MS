6fbf4a7349d0d4ee0a7da0cbc15abf16af845b1c
Margarita Dagla <margaritadagla@hotmail.com>
2023-02-18 10:22:15 +0000

    update routes to include reviews and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root to: "flats#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Adding an 'about' controller method
-  resources :flats
+  resources :flats do
+    resources :reviews, only [:new, :create]
+    resources :bookings, only [:new, :create, :edit, :update]
+  end
+  resources :bookings, only [ :destroy ]
+  resources :reviews, only [ :destroy ]
   # Defines the root path route ("/")
   # root "articles#index"
 end
