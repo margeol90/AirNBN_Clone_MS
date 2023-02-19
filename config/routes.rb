e34f87c1a7306a5e5a01fb81ae551aced08904fc
Babajide Muritala <jide142000@yahoo.com>
2023-02-19 12:38:30 +0100

    resolving conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,15 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'reviews/new'
-=======
   get 'bookings/new'
->>>>>>> master
   devise_for :users
   root to: "flats#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Adding an 'about' controller method
   resources :flats do
-<<<<<<< HEAD
-    resources :reviews, only: [:new, :create]
-  end
-=======
     resources :reviews, only: %i[new create]
     resources :bookings, only: %i[create edit update]
   end
   resources :bookings, only: [:destroy]
   resources :reviews, only: [:destroy]
->>>>>>> master
   # Defines the root path route ("/")
   # root "articles#index"
   get    "pages/my_flats", to: "pages#my_flats", as: :my_flats
