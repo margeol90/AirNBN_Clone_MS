02e38ef617c9e8aa0ea480540dfb3b3717df6f60
Francisco Lago <francisco.lago@gmail.com>
2023-02-22 20:47:45 +0100

    all merging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_22_181115) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_181115) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
