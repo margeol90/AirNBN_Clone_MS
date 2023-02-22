03c42a78a470ec197d90ac75c5eacf766e24bce5
Babajide Muritala <jide142000@yahoo.com>
2023-02-22 17:27:36 +0100

    migration to add rating to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_141437) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_162453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_141437) do
     t.bigint "booking_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
