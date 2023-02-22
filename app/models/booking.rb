affce8bb9f05c46b8ad100496473af308a560477
Francisco Lago <francisco.lago@gmail.com>
2023-02-22 18:35:45 +0100

    Fixed booking adults/children error, fixed front-end of show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,7 +5,7 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :adults, :children, presence: true
+  validates :adults, :children, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validate :end_date_after_start_date
 
   private
@@ -13,8 +13,6 @@ class Booking < ApplicationRecord
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
 
-    if end_date < start_date
-      errors.add(:end_date, "must be after the start date")
-    end
+    errors.add(:end_date, "must be after the start date") if end_date < start_date
   end
 end
